191c9db7fa8b76312d4550871352f54858a456ec
Bruno Miguens <bruno.miguens@olx.com>
2017-05-31 14:23:11 +0100

    Fixed an issue found by a static analyzer.

--- BMSegmentedControl/BMSegmentedControl.swift
+++ BMSegmentedControl/BMSegmentedControl.swift
@@ -204,7 +204,7 @@ class BMSegmentedControl: UIControl {
     }
     
     fileprivate func getTextX(_ itemWidth: CGFloat, textWidth: CGFloat) -> CGFloat {
-        var iconWidth:CGFloat = withIcon ? 16.0 : 0.0
+        var iconWidth: CGFloat = withIcon ? 16.0 : 0.0
         
         let avg = iconWidth + textWidth
         let space: CGFloat = (itemWidth - avg) / 2
